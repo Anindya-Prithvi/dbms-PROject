293d6742be74339c9ee2838194bd5ad3b05a15e8
AndyCandy <anindya20024@iiitd.ac.in>
2022-04-12 23:22:07 +0530

    post get query body express

--- backend/index.js
+++ backend/index.js
@@ -3,18 +3,57 @@ var dotenv = require('dotenv');
 
 var express = require('express');
 const app = express();
+const port = 3000;
+const DATABASE_NAME = "BDSM";
 
-var con = mysql.createConnection({
+var con_user_1 = mysql.createConnection({
     host: "localhost",
     port: 3306,
-    user: dotenv.config().parsed.DB_USER,
-    password: dotenv.config().parsed.DB_PASSWORD,
+    user: dotenv.config().parsed.DB_USER_1,
+    password: dotenv.config().parsed.DB_PASSWORD_1,
 });
 
-con.connect(function (err) {
+con_user_1.connect(function (err) {
     if (err) throw err;
-    console.log("Connected!");
+    console.log("Connected with privilege 1!");
 });
 
+var con_user_2 = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: dotenv.config().parsed.DB_USER_2,
+    password: dotenv.config().parsed.DB_PASSWORD_2,
+});
 
+con_user_2.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected with privilege 2!");
+});
 
+con_user_1.query(`USE ${DATABASE_NAME}`);
+con_user_2.query(`USE ${DATABASE_NAME}`);
+
+app.get('/', (req, res) => {
+    console.log(req.body); //works with POST
+    console.log(req.query); //works with GET 
+    res.send("Do not ping root lol");
+})
+
+app.get('/register', (req, res) => {
+    con_user_1.query(`SELECT 1`, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+    });
+    res.send('Working');
+})
+
+app.post('/register', (req, res) => {
+    con_user_1.query(`-- INSERT INTO ${req.body}`, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        console.log("posting");
+        res.send("Registered!!");
+    })
+})
+
+app.listen(process.env.PORT || port);
