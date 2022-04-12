18255f40f0004f8c577baf01a21c4b7d97e1b629
Tanishk Goyal <goyaltanishk02@gmail.com>
2022-04-12 21:20:59 +0530

    sorry anindya

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,18 @@
-console.log("HIIIIIIIIIIIIIIIIIIIIIII")
+var mysql = require('mysql');
+var dotenv = require('dotenv');
+var express = require('express');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    port: 3305,
+    user: dotenv.config().parsed.DB_USER,
+    password: dotenv.config().parsed.DB_PASSWORD,
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+
+
