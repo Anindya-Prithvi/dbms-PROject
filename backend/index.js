b49f1965f2c22d165a2bbdb26d28a1cb6041d4bb
AndyCandy <anindya20024@iiitd.ac.in>
2022-04-20 01:30:08 +0530

    static pages reset

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use('/static', express.static('dist'))
 
 const secret = process.env.secret || dotenv.config().parsed.secret;
 function validateCookies(req, res, next) {
