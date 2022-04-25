0b64c91b7eea13cfe4d782e66241a52ccd212d4e
Vibhu Dubey <vibhu20150@iiitd.ac.in>
2022-04-25 05:46:21 +0530

    basic cards

--- frontend/src/app/saving-passbook-send-money-credit-card/saving-passbook-send-money-credit-card.component.ts
+++ frontend/src/app/saving-passbook-send-money-credit-card/saving-passbook-send-money-credit-card.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { axios } from '../../utilities/axios';
 
 @Component({
   selector: 'app-saving-passbook-send-money-credit-card',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SavingPassbookSendMoneyCreditCardComponent implements OnInit {
 
+  cardNo = "xxxx xxxx xxxx 1234";
+  expiry = "xx/xx";
+  name = "Vibster";
+
+
+  transactionCreds = new FormGroup({
+    toAccount: new FormControl('', [Validators.required]),
+    toAccountType: new FormControl('', [Validators.required]),
+    cvv: new FormControl('', [Validators.required]),
+    amount: new FormControl('', [Validators.required])
+  })
+
+  @Input()
+  logintype: string | undefined = undefined; 
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    console.log(this.transactionCreds.value);
+    //use login endpoint
+    axios.post('/api/v1/' + (this.logintype === 'user' ? 'login' : 'managerlogin'), this.transactionCreds.value).then(response => {
+      console.log(response.data);
+      console.log(response);
+
+      // localStorage.setItem('accesscookie', response.data);
+      // if (response.data === "correct") { sessionStorage.setItem('login', 'true') };
+      // window.location.reload(); //comment out at debugging
+    })
   }
 
 }
