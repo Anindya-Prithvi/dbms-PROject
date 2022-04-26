51654d9daffa1aeb21465ad43e6b06263a7aba78
Tanishk Goyal <goyaltanishk02@gmail.com>
2022-04-26 22:43:16 +0530

    First View

--- SQL_scripts/balance_updation_trigger.sql
+++ SQL_scripts/balance_updation_trigger.sql
@@ -9,12 +9,15 @@ BEGIN
 UPDATE savingsaccount
 SET balance = balance + new.amount 
 WHERE new.toAccount = savingsaccount.accountNo;
+
 UPDATE currentaccount
 SET balance = balance + new.amount
 WHERE new.toAccount = currentaccount.accountNo;
+
 UPDATE loanaccount
 SET amountDue = amountDue + new.amount
 WHERE new.toAccount = loanaccount.accountNo;
+
 UPDATE creditcardaccount
 SET amountDue = amountDue + new.amount
 WHERE new.toAccount = creditcardaccount.accountNo;
@@ -22,12 +25,15 @@ WHERE new.toAccount = creditcardaccount.accountNo;
 UPDATE savingsaccount
 SET balance = balance - new.amount 
 WHERE NEW.fromAccserialNo =  savingsaccount.serialNo AND NEW.fromAcccustomerId = savingsaccount.customerId;
+
 UPDATE currentaccount
 SET balance = balance - new.amount 
 WHERE NEW.fromAccserialNo =  currentaccount.serialNo AND NEW.fromAcccustomerId = currentaccount.customerId;
+
 UPDATE loanaccount
 SET amountDue = amountDue - new.amount
 WHERE NEW.fromAccserialNo =  loanaccount.serialNo AND NEW.fromAcccustomerId = loanaccount.customerId;
+
 UPDATE creditcardaccount
 SET amountDue = amountDue - new.amount
 WHERE NEW.fromAccserialNo =  creditcardaccount.serialNo AND NEW.fromAcccustomerId = creditcardaccount.customerId;
