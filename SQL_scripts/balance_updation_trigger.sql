5170a2bedac07f522c7e9d3d48024b428bb64bfe
AndyCandy <anindya20024@iiitd.ac.in>
2022-04-26 23:05:22 +0530

    minor updates to times

--- SQL_scripts/balance_updation_trigger.sql
+++ SQL_scripts/balance_updation_trigger.sql
@@ -1,9 +1,7 @@
-USE bdsm;
-DROP TRIGGER after_transaction_insert;
+DROP TRIGGER IF EXISTS after_transaction_insert;
 
-DELIMITER $$ 
 create TRIGGER after_transaction_insert AFTER INSERT
-on bdsm.transaction
+on transaction
 FOR EACH ROW
 BEGIN
 UPDATE savingsaccount
@@ -37,20 +35,3 @@ WHERE NEW.fromAccserialNo =  loanaccount.serialNo AND NEW.fromAcccustomerId = lo
 UPDATE creditcardaccount
 SET amountDue = amountDue - new.amount
 WHERE NEW.fromAccserialNo =  creditcardaccount.serialNo AND NEW.fromAcccustomerId = creditcardaccount.customerId;
-END $$
-
-
-
-
--- IF 
--- ((NEW.TOA > (SEL balance from bdsm.savingsaccount as sa where (NEW.fromAccserialNo =  sa.serialNo AND NEW.fromAcccustomerId = sa.customerId)))
--- OR
--- (NEW.amount > (SELECT balance from bdsm.currentaccount as ca where (NEW.fromAccserialNo =  ca.serialNo AND NEW.fromAcccustomerId = ca.customerId)))
--- OR
--- (NEW.amount > (SELECT creditLimit - creditSpent from bdsm.creditcardaccount as cca where (NEW.fromAccserialNo =  cca.serialNo AND NEW.fromAcccustomerId = cca.customerId)))
--- OR
--- (NEW.amount > (SELECT principal from bdsm.loanaccount as la where (NEW.fromAccserialNo =  la.serialNo AND NEW.fromAcccustomerId = la.customerId))))
-
--- THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Insufficient Balance.';
--- END IF; $$
--- DELIMITER ; 
