8b67aaf7fe0d9cc3196063926e6b1f07d5ee9117
AndyCandy <anindya20024@iiitd.ac.in>
2022-04-27 16:41:23 +0530

    dynamic lazy loading

--- frontend/src/app/transactions-graph/transactions-graph.component.ts
+++ frontend/src/app/transactions-graph/transactions-graph.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-const Desmos = require('src/assets/calculator');
 
 @Component({
   selector: 'app-transactions-graph',
@@ -7,22 +6,44 @@ const Desmos = require('src/assets/calculator');
   styleUrls: ['./transactions-graph.component.css']
 })
 export class TransactionsGraphComponent implements OnInit {
+  myScriptElement: HTMLScriptElement;
+  Desmos: any
 
-  constructor() { }
+  constructor() {
+    this.myScriptElement = document.createElement("script");
+    this.myScriptElement.src = "https://www.desmos.com/api/v1.7/calculator.js?apiKey=dcb31709b452b1cf9dc26972add0fda6";
+    document.body.appendChild(this.myScriptElement);
+
+  }
 
   ngOnInit(): void {
-    var elt = document.getElementById('calculator');
-    var calculator = Desmos.GraphingCalculator(elt);
-    // instructions to populate
-    // 1. see the line below, edit latex: 'someedits here'
-    // I have already fille with points, if you maintain the syntax yayyy
-    calculator.setExpression({ id: 'count vs epoch', latex: '([11111111,1000000],[-1000,1000])' });
-    calculator.setMathBounds({
-      left: -10000000000,
-      right: 10000000000,
-      bottom: -10000000,
-      top: 10000000
-    });
+
+    this.myScriptElement.onload = () => {
+      this.Desmos = (<any>window).Desmos;
+      console.log('loaded');
+      document.getElementsByTagName('head')[0].appendChild(this.myScriptElement);
+      var elt = document.getElementById('calculator');
+      var calculator = this.Desmos.GraphingCalculator(elt);
+      // instructions to populate
+      // 1. see the line below, edit latex: 'someedits here'
+      // I have already fille with points, if you maintain the syntax yayyy
+      // dont do ched chaad above lol
+      calculator.setExpression({ id: 'count vs epoch', latex: '([11111111,1000000],[-1000,1000])' });
+      calculator.setMathBounds({
+        left: -10000000000,
+        right: 10000000000,
+        bottom: -10000000,
+        top: 10000000
+      });
+
+
+    }
+
+    console.log('rest');
+
+
+
+
   }
 
 }
