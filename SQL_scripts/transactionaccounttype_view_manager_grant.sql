0c96fd930ddeb5af44917f08e3ed1cd5652b4c1a
AndyCandy <anindya20024@iiitd.ac.in>
2022-04-27 23:37:24 +0530

    rename -const

--- SQL_scripts/transactionaccounttype_view_manager_grant.sql
+++ SQL_scripts/transactionaccounttype_view_manager_grant.sql
@@ -3,36 +3,40 @@ drop view if exists allloanaccounttransactions;
 drop view if exists allcreditcardaccounttransactions;
 drop view if exists allcurrentaccounttransactions;
 
-create view allSavingsAccountTransactions as 
-select txnId,timeOfTransaction, toAccount, amount, savingsaccount.accountNo, savingsaccount.customerId, 'DEBIT'
+create view allsavingsaccounttransactions as 
+select txnId,timeOfTransaction, toAccount, amount, savingsaccount.accountNo, savingsaccount.customerId, 'DEBIT' as creditOrDebit
 from transaction, savingsaccount
 where transaction.fromAccserialNo = savingsaccount.serialNo AND transaction.fromAcccustomerId = savingsaccount.customerId UNION 
-select txnId,timeOfTransaction, toAccount, amount, savingsaccount.accountNo, savingsaccount.customerId, 'CREDIT'
+select txnId,timeOfTransaction, toAccount, amount, savingsaccount.accountNo, savingsaccount.customerId, 'CREDIT' creditOrDebit
 from transaction, savingsaccount
 WHERE transaction.toAccount = savingsaccount.accountNo;
 
-create view allLoanAccountTransactions as 
-select txnId,timeOfTransaction, toAccount, amount, loanaccount.accountNo, loanaccount.customerId, 'DEBIT'
+create view allloanaccounttransactions as 
+select txnId,timeOfTransaction, toAccount, amount, loanaccount.accountNo, loanaccount.customerId, 'DEBIT' as creditOrDebit
 from transaction, loanaccount
 where transaction.fromAccserialNo = loanaccount.serialNo AND transaction.fromAcccustomerId = loanaccount.customerId UNION 
-select txnId,timeOfTransaction, toAccount, amount, loanaccount.accountNo, loanaccount.customerId, 'CREDIT'
+select txnId,timeOfTransaction, toAccount, amount, loanaccount.accountNo, loanaccount.customerId, 'CREDIT' as creditOrDebit
 from transaction, loanaccount
 WHERE transaction.toAccount = loanaccount.accountNo;
 
 create view allcreditcardaccounttransactions as 
-select txnId,timeOfTransaction, toAccount, amount, creditcardaccount.accountNo, creditcardaccount.customerId, 'DEBIT'
+select txnId,timeOfTransaction, toAccount, amount, creditcardaccount.accountNo, creditcardaccount.customerId, 'DEBIT' as creditOrDebit
 from transaction, creditcardaccount
 where transaction.fromAccserialNo = creditcardaccount.serialNo AND transaction.fromAcccustomerId = creditcardaccount.customerId UNION 
-select txnId,timeOfTransaction, toAccount, amount, creditcardaccount.accountNo, creditcardaccount.customerId, 'CREDIT'
+select txnId,timeOfTransaction, toAccount, amount, creditcardaccount.accountNo, creditcardaccount.customerId, 'CREDIT' as creditOrDebit
 from transaction, creditcardaccount
 WHERE transaction.toAccount = creditcardaccount.accountNo;
 
 create view allcurrentaccounttransactions as 
-select txnId,timeOfTransaction, toAccount, amount, currentaccount.accountNo, currentaccount.customerId, 'DEBIT'
+select txnId,timeOfTransaction, toAccount, amount, currentaccount.accountNo, currentaccount.customerId, 'DEBIT' as creditOrDebit
 from transaction, currentaccount
 where transaction.fromAccserialNo = currentaccount.serialNo AND transaction.fromAcccustomerId = currentaccount.customerId UNION 
-select txnId,timeOfTransaction, toAccount, amount, currentaccount.accountNo, currentaccount.customerId , 'CREDIT'
+select txnId,timeOfTransaction, toAccount, amount, currentaccount.accountNo, currentaccount.customerId , 'CREDIT' as creditOrDebit
 from transaction, currentaccount
 WHERE transaction.toAccount = currentaccount.accountNo;
 
--- grant select on customerInfo to 'bankadmin';
+-- grant select on customerInfo to 'dummyUser2';
+
+-- select * from allSavingsAccountTransactions;
+
+-- select * from allSavingsAccountTransactions;
