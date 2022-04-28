628528a333ed27e97c5dc066628a508e222a1a01
Vibhu Dubey <vibhu20150@iiitd.ac.in>
2022-04-28 18:09:42 +0530

    minor fix

--- frontend/src/app/current-transactions-view/current-transactions-view.component.ts
+++ frontend/src/app/current-transactions-view/current-transactions-view.component.ts
@@ -70,7 +70,7 @@ export class CurrentTransactionsViewComponent implements OnInit {
           time: element[3],
           toAccount: element[4],
           fromAccount: element[5],
-          creditDebit: element[8]
+          creditDebit: element[6]
         });
         // TRANSACTION_DATA =  TRANSACTION_DATA.slice(1, );
         this.dataSource.data = TRANSACTION_DATA;
